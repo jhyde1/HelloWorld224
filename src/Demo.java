1c37c8b57f7013ebf5f09c60fde836fdcbbe97e8
John Hyde <jhyde2@zagmail.gonzaga.edu>
2019-08-29 14:37:45 -0700

    add another println

--- src/Demo.java
+++ src/Demo.java
@@ -3,5 +3,6 @@ public class Demo
     public static void main(String[] args)
     {
         System.out.println("My second hello world!");
+        System.out.println("another print line");
     }
 }
